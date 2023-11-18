aeaadc63aaadb8067ef48b0cc1c63e5ffac4110b
@rduino4141! <j.huanca4141@gmail.com>
2023-11-18 17:26:01 -0500

    add profile for userName and target v2

--- src/main/java/com/ExcludedHouse/speakHome/security/api/rest/RoleController.java
+++ src/main/java/com/ExcludedHouse/speakHome/security/api/rest/RoleController.java
@@ -5,6 +5,7 @@ import com.ExcludedHouse.speakHome.security.mapping.RoleMapper;
 import com.ExcludedHouse.speakHome.security.resource.CreateRoleResource;
 import com.ExcludedHouse.speakHome.security.resource.RoleResource;
 import com.ExcludedHouse.speakHome.security.resource.UpdateRoleResource;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/api/v1/security/roles", produces = "application/json")
+@Tag(name = "Roles", description = "Create, read, update and delete roles")
 public class RoleController {
   private final RoleService roleService;
 
